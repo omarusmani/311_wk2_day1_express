97023446f33b06aa3623d974b69989e91d81082d
omarusmani <omarusmani3@hotmail.com>
2022-07-21 20:20:50 -0500

    done with using param :userId

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const port = process.env.PORT || 4000
 
 const { users } = require('./state')
 
-let count=users.length
+let count=users.length+1
 /* BEGIN - create routes here */
 app.use(bodyParser.json());
 app.get("/users", (req,res)=>res.json(users));
-app.get("/users/:id", (req,res)=>res.json(users[req.params.id-1]));
+app.get("/users/:userId", (req,res)=>{
+  const person =users.find((user)=> user._id===Number(req.params.userId));
+  console.log(person)
+  res.json(person)}
+  );
 app.post("/users",(req,res)=>{
   // let newp=
   // {
@@ -27,24 +31,30 @@ app.post("/users",(req,res)=>{
   //   "avatar": "https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"
   // })
   console.log(req.body)
-  // users.push({...req.body,
-  //              ..._id:count,})
+  let newuser ={
+    _id:count,
+    ...req.body,}
+  users.push(newuser)
+  count++
   res.json(users)});
 
-app.put("/users/1",(req,res)=>{
+app.put("/users/:userId",(req,res)=>{
+  const person =users.find((user)=> user._id===Number(req.params.userId));
+  const index =users.findIndex((user)=> user._id ===Number(req.params.userId));
+  console.log(person,index)
   let newp={
-    ...users[0],
-    ...{
-      "_id":"4",
-    }
+    ...person,
+    ...req.body
   }
   console.log(newp)
-  users.slice(0,1,newp)
+  users.splice(index,1,newp)
   res.json(users)
 });
-app.delete("/users/1",(req,res)=>{
-  users.splice(0,1)
-  res.json(users)
+app.delete("/users/:userId",(req,res)=>{
+  const person =users.find((user)=> user._id===Number(req.params.userId));
+  const index =users.findIndex((user)=> user._id ===Number(req.params.userId));
+  users.splice(index,1,{...person,isActive:false})
+  res.send("deleted")
 })
 
 /* END - create routes here */
